8272afee56371cac7b1f916da04d71457824a278
Gustavo-Lackeski <glackeski@gmail.com>
2017-06-21 13:10:00 -0300

    comeco

--- Processor/src/processor/Processor.java
+++ Processor/src/processor/Processor.java
@@ -3,7 +3,10 @@ package processor;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Vector;
+import java.util.Arrays;
+import java.util.List;
 
 class RegisterStatus{
     public int qi;
@@ -31,14 +34,25 @@ class ReorderBuffer{
 }
 
 public class Processor {
+    private final int  N_RegisterStatus = 32;
+    private int pc = 0;
+    private RegisterStatus registerStatus[] = new RegisterStatus[N_RegisterStatus];
+    private Vector<Command> commands = new Vector();
+    private Vector<ReorderBuffer> ro = new Vector();
+    private ArrayList<Command> filaDeInstrucoes = new ArrayList<>();
+    private List reservationStationsSoma = Arrays.asList(new ReservationStation[3]);
+    private List reservationStationsMultiplicacao = Arrays.asList(new ReservationStation[2]);
+    private List reservationStationsMemoria = Arrays.asList(new ReservationStation[5]);
     
-    private RegisterStatus registerStatus[] = new RegisterStatus[32];
-    private Vector<Command> commands;
-    private Vector<ReorderBuffer> ro;
-    private ReservationStation reservationStations[] = new ReservationStation[12];
+    //variaveis temporarias, necessarias para simular paralelismo
+    private RegisterStatus registerStatusTemp[] = new RegisterStatus[N_RegisterStatus];
+    private Vector<ReorderBuffer> roTemp = new Vector();
+    private ArrayList<Command> filaDeInstrucoesTemp = new ArrayList<>();
+    private List reservationStationsSomaTemp = Arrays.asList(new ReservationStation[3]);
+    private List reservationStationsMultiplicacaoTemp = Arrays.asList(new ReservationStation[2]);
+    private List reservationStationsMemoriaTemp = Arrays.asList(new ReservationStation[5]);
     
     private void readFile(){
-        
         Command.setMap();
         String filename = "program.txt";
         try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
@@ -53,6 +67,51 @@ public class Processor {
     
     public Processor(){
         readFile();
+    }
+    public void issue(){
+        //colocar da memoria na fila
+        filaDeInstrucoesTemp.add(commands.get(pc/4)); //tem que ser pc/4
+        if(!filaDeInstrucoes.isEmpty()){
+            Command co = filaDeInstrucoes.get(0);
+            //ver se a estacao de reserv correspondente estar vazia, e adicionar
+        }
+    }
+    /*
+    public void issue (Command New_Instruction, int Instruction_index)
+    {
+        int i;
+        for (i = 0; i < N_ReservationStation; i++)
+            if (!reservationStations[i].busy)
+                break;
+        
+        if (i < N_ReservationStation)    //Existe uma estação de reserva vazia.
+        {
+            reservationStations[i].busy = true;
+            New_Instruction.T_Dest = i;
+            if (New_Instruction.isR())
+            {
+                reservationStations[i].qj = FindCorrespondingSource(New_Instruction.rs, Instruction_index);
+                reservationStations[i].qk = FindCorrespondingSource(New_Instruction.rt, Instruction_index);
+            }
+            
+            if (New_Instruction.isI())
+            {
+                reservationStations[i].qj = FindCorrespondingSource(New_Instruction.rd, Instruction_index);
+            }
+            
+        }
+       
+    }*/
+    
+    //Percorre todos os índices de 0 a Instruction_index - 1, procurando a instrução com maior índice
+    //de modo que Source_Register seja o registrador de destino, e retorna o índice da estação de reserva
+    //correspondente.
+    public int FindCorrespondingSource (int Source_Register, int Instruction_index)
+    {
+        for (int i = Instruction_index - 1; i >= 0; i++)
+            if (commands.get(i).rd == Source_Register)
+                return commands.get(i).T_Dest;
+        return Source_Register;    
     }
     
     public void nextClock(){
---
+++ Processor/test/NovoEmptyJUnitTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import processor.Processor;
+
+/**
+ *
+ * @author G1511NEW
+ */
+public class NovoEmptyJUnitTest {
+    @Test
+    public void NovoEmptyJUnitTest() {
+        Processor p = new Processor();
+        while(true){
+            
+        }
+    }
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
